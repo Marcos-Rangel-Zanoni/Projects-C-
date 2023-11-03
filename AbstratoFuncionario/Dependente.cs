f44bc4240d12efe47dd1b9bf0f4306f75cec6e70
Marcos-Rangel-Zanoni <72946366+Marcos-Rangel-Zanoni@users.noreply.github.com>
2023-11-03 09:10:36 -0300

    comit

---
+++ AbstratoFuncionario/Dependente.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstratoFuncionario
+{
+    class Dependente
+    {
+        private int codigo;
+        private string nome;
+        private int idade;
+
+        public Dependente(int codigo, string nome, int idade)
+        {
+            Codigo = codigo;
+            Nome = nome;
+            Idade = idade;
+        }
+        public void Mostrar()
+        {
+            Console.WriteLine("Código: " + Codigo + "\tNome:" + Nome + "\tIdade: " + Idade);
+        }
+
+        public int Codigo
+        {
+            get { return codigo; }
+            set { codigo = value; }
+        }
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public int Idade
+        {
+            get { return idade; }
+            set { idade = value; }
+        }
+
+        public bool verificarMaiorIdade()
+        {
+            if (Idade > 18)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ AbstratoFuncionario/Funcionario.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstratoFuncionario
+{
+    abstract class Funcionario //classe base-superclasse
+    {
+        protected int codigo;
+        protected string nome;
+        protected double salario;
+        public abstract double CalcularSalario(int diasUteis);
+        
+        private List<Dependente> dp;
+
+        public virtual void ListarDependente()
+        {
+            dp = new List<Dependente>();
+            foreach (Dependente dependente in dp)
+                Console.WriteLine(dependente);
+        }
+        public void AdicionarDependente(Dependente novoDep)
+        {
+            dp.Add(novoDep);
+        }
+        public void RemoverDependentesMaioridade(int codigo)
+        {
+            for (int i = 0; i < dp.Count; i++)
+            {
+                Dependente depend = dp.ElementAt<Dependente>(i);
+                if (depend.Codigo == codigo)
+                    dp.Remove(depend);
+            }
+        }
+        public virtual void Mostrar()//polimorfismo
+        {
+            Console.WriteLine("Código: " + Codigo +
+                "\tNome:" + Nome + "\tSalário: " + Salario);
+        }
+        public Funcionario(int codigo, string nome, double salario)
+        {
+            Codigo = codigo;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public int Codigo
+        {
+            get { return this.codigo; }
+            set { this.codigo = value; }
+        }
+        public string Nome
+        {
+            get { return this.nome; }
+            set { this.nome = value; }
+        }
+        public double Salario
+        {
+            get { return this.salario; }
+            set { this.salario = value; }
+        }
+    }
+}
